bf59f41dbe46f11c25ce676ee9c90e3654355df4
robcholz <84130577+robcholz@users.noreply.github.com>
2024-12-23 16:53:44 -0500

    [refactor] updated server api address for production

--- src/constant.rs
+++ src/constant.rs
@@ -2,4 +2,4 @@
 pub const SERVER_ADDRESS: &str = "http://localhost:8000";
 
 #[cfg(not(debug_assertions))]
-pub const SERVER_ADDRESS: &str = "https://oreminder.com";
+pub const SERVER_ADDRESS: &str = "https://oreminder.com/api";
